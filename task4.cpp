4e495692eb0175d52f7fa8ae6ed919b7ef845a1c
Muhammad-Mateen207 <2024CS207@student.uet.edu.pk>
2024-12-08 22:14:54 +0500

    task4.cpp

--- task4.cpp
+++ task4.cpp
@@ -1,106 +1,61 @@
-// #include<iostream>
-// #include<fstream>
-// #include<string>
-// using namespace std;
-
-// int returnIndex(string array, char a);
-// string filterElements(string array, string line);
-
-// int main()
-// {
-//    string array = "abcdefghijklmnopqrstuvwxyz";
-//    string line;
-
-//    fstream file;
-//    file.open("TASK4_cp.txt", ios::in);
-//    getline(file,line);
-//    file.close();
-
-//    string filteredArray = filterElements(array,line);
-
-//    fstream newFile;
-//    newFile.open("TASK4_cp.txt", ios::app);
-//    newFile << endl << filteredArray ;
-//    newFile.close();
-
-// }
-// int returnIndex(string array, char a)
-// {
-//     for (int i=0; i < array.length(); i++)
-//     {
-//         if (array[i]=a)
-//         {
-//             return i;
-//         }
-//     }
-//     return -1;
-// }
-// string filterElements(string array, string line)
-// {
-//     string result;
-//     for(int i=0; i <array.length(); i++)
-//     {
-//         char currentChar = array[i];
-//         bool shouldAdd = true;
-
-//         for(int j=0; j<array.length();j++)
-//         {
-//             if(currentChar = line[j])
-//             {
-//                 shouldAdd = false;
-//                 break;
-//             }
-//         }
-//         if(shouldAdd)
-//         {
-//             result = result + currentChar;
-//         }
-//     }
-//     return result;
-// }
-
 #include<iostream>
 #include<fstream>
 #include<string>
 using namespace std;
-string readfile();
-void append(string missing);
-int main() 
+
+int returnIndex(string array, char a);
+string filterElements(string array, string line);
+
+int main()
 {
-    string record=readfile();
-    string letters="abcdefghijklmnopqrstuvwxyz";
-    string missing="";
-    for (int i=0; i<letters.length(); i++) 
+   string array = "abcdefghijklmnopqrstuvwxyz";
+   string line;
+
+   fstream file;
+   file.open("TASK4_cp.txt", ios::in);
+   getline(file,line);
+   file.close();
+
+   string filteredArray = filterElements(array,line);
+
+   fstream newFile;
+   newFile.open("TASK4_cp.txt", ios::app);
+   newFile << endl << filteredArray ;
+   newFile.close();
+
+}
+int returnIndex(string array, char a)
+{
+    for (int i=0; i < array.length(); i++)
     {
-        bool found=false;
-        for (int j=0; j<record.length(); j++) 
+        if (array[i]=a)
         {
-            if(letters[i]==record[j]) 
+            return i;
+        }
+    }
+    return -1;
+}
+string filterElements(string array, string line)
+{
+    string result;
+    for(int i=0; i <array.length(); i++)
+    {
+        char currentChar = array[i];
+        bool shouldAdd = true;
+
+        for(int j=0; j<array.length();j++)
+        {
+            if(currentChar = line[j])
             {
-                found = true;
+                shouldAdd = false;
                 break;
             }
         }
-        if(found==false) 
+        if(shouldAdd)
         {
-            missing=missing+letters[i];
+            result = result + currentChar;
         }
     }
-    append(missing);
+    return result;
 }
-string readfile() 
-{
-    string line;
-    fstream file;
-    file.open("TASK4_cp.txt", ios::in);
-    getline(file, line);
-    file.close();
-    return line;
-}
-void append(string missing) 
-{
-    fstream file;
-    file.open("TASK4_cp.txt", ios::app);
-    file << endl << missing;
-    file.close();
-}
+
